29396b98f7d0f0441907db3fe20ef4b6b7daa88d
Seunglee Choi <sc9ny@vt.edu>
2018-10-02 00:13:42 -0400

    working on basic frontend

--- 100QA/QA_100/QA_100/urls.py
+++ 100QA/QA_100/QA_100/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+from QA import views as general_view
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
+    path('', general_view.home, name='home')
     #path ('signup/',accounts_views.signup, name ='signup')
 ]
--- 100QA/QA_100/account/urls.py
+++ 100QA/QA_100/account/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from django.contrib.auth import views as auth_view
 from account import views
 
+app_name='account'
 urlpatterns = [
     path('signup/', views.signup, name = 'signup'),
     path('logout/', auth_view.LogoutView.as_view(), name='logout'),
-    path('login/', auth_view.LoginView.as_view(template_name='account/login.html')),
+    path('login/', auth_view.LoginView.as_view
+     (template_name='account/login.html'), name = 'login'),
 ]
